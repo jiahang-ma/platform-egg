f4e82e2d58af9d5a931a5d39140e6d0d48aee606
hang1989 <419917737@qq.com>
2019-09-29 11:57:22 +0800

    app(chore):init

---
+++ config/config.default.js
@@ -0,0 +1,18 @@
+exports.keys = 'kylin-ma';
+exports.view = {
+    defaultViewEngine: 'nunjucks',
+    mapping: {
+        '.tpl': 'nunjucks'
+    },
+};
+exports.news = {
+    pageSize: 5,
+    serverUrl: 'https://hacker-news.firebaseio.com/v0',
+};
+
+exports.middleware = ['robot'];
+exports.robot = {
+    ua: [/curl/i, /Baiduspider/i],
+};
+
+
